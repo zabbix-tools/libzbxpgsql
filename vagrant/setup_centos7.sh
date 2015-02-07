823bfbc7f664e1cc71b6a8775033977cb7a58dc9
Ryan Armstrong <ryan@cavaliercoder.com>
2015-02-07 22:15:23 +0800

    Added Ubuntu 12.04 Precise Vagrant box

---
+++ vagrant/setup_centos7.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+BULLET="==>"
+cd /vagrant
+
+# Install zabbix sources
+echo -e "${BULLET} Downloading and extracting Zabbix sources..."
+[[ -f zabbix-2.4.3.tar.gz ]] || sudo -Eu vagrant curl -LO http://sourceforge.net/projects/zabbix/files/ZABBIX%20Latest%20Stable/2.4.3/zabbix-2.4.3.tar.gz
+[[ -f zabbix-2.4.3/README ]] || sudo -Eu vagrant tar -xzf zabbix-2.4.3.tar.gz
+
+# Install Zabbix, PostgreSQL and build tools
+echo -e "${BULLET} Installing Zabbix, PostgreSQL and build tools..."
+rpm -qa | grep pgdg >/dev/null || yum localinstall -y --nogpgcheck http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
+rpm -qa | grep zabbix-release >/dev/null || yum localinstall -y --nogpgcheck http://repo.zabbix.com/zabbix/2.4/rhel/7/x86_64/zabbix-release-2.4-1.el7.noarch.rpm
+yum install -y --nogpgcheck make gcc libtool automake autoconf postgresql94-server postgresql94-devel phpPgAdmin zabbix-agent
+
+# Build module
+echo -e "${BULLET} Building the libzbxpgsql agent module..."
+libtoolize >/dev/null
+aclocal >/dev/null
+autoheader >/dev/null
+automake --add-missing >/dev/null
+autoreconf >/dev/null
+./configure >/dev/null && make >/dev/null
+
+# Configure PostgreSQL
+echo -e "${BULLET} Configuring PostgreSQL server..."
+/usr/pgsql-9.4/bin/postgresql94-setup initdb
+cat > /var/lib/pgsql/9.4/data/pg_hba.conf <<EOL
+local   all             all                                     trust
+host    all             all             127.0.0.1/32            trust
+host    all             all             ::1/128                 trust
+EOL
+systemctl enable postgresql-9.4
+systemctl start postgresql-9.4
+
+# Configure phpPgAdmin
+echo -e "${BULLET} Configuring phpPgAdmin web console..."
+cat > /etc/httpd/conf.d/phpPgAdmin.conf <<EOL
+Alias /phpPgAdmin /usr/share/phpPgAdmin
+<Location /phpPgAdmin>
+    <IfModule mod_authz_core.c>
+        # Apache 2.4
+        Require all granted
+    </IfModule>
+    <IfModule !mod_authz_core.c>
+        # Apache 2.2
+        Order deny,allow
+        Allow from all
+    </IfModule>
+</Location>
+EOL
+
+sed -i \
+    -e "s/\$conf\['extra_login_security'\] = .*;/\$conf\['extra_login_security'\] = false;/" \
+    -e "s/\$conf\['servers'\]\[0\]\['host'\] = .*/\$conf\['servers'\]\[0\]\['host'\] = 'localhost';/" \
+    -e "s/\$conf\['owned_only'\] = .*/\$conf\['owned_only'\] = false;/" \
+    /etc/phpPgAdmin/config.inc.php
+systemctl enable httpd
+systemctl start httpd
+
+# Configure Zabbix Agent
+echo -e "${BULLET} Installing libzbxpgsql..."
+echo "LoadModule=libzbxpgsql.so" > /etc/zabbix/zabbix_agentd.d/libzbxpgsql.conf
+
+# Install add link to agent modules for built libzbxpgsql module
+[[ -d /usr/lib64/modules ]] || mkdir /usr/lib64/modules
+[[ -L /usr/lib64/modules/libzbxpgsql.so ]] || ln -s /vagrant/src/.libs/libzbxpgsql.so /usr/lib64/modules/libzbxpgsql.so
+
+# MOTD
+echo -e "${BULLET} Configuring message of the day..."
+cat > /etc/motd <<MOTD
+  _ _ _        _                               _ 
+ | (_) |__ ___| |____  ___ __   __ _ ___  __ _| |
+ | | | '_ \\_  / '_ \\ \\/ / '_ \\ / _\` / __|/ _\` | |
+ | | | |_) / /| |_) >  <| |_) | (_| \\__ \\ (_| | |
+ |_|_|_.__/___|_.__/_/\\_\\ .__/ \\__, |___/\\__, |_|
+                        |_|    |___/        |_|  
+
+            DΣVΣᄂӨPMΣПƬ VΛGЯΛПƬ BӨX
+
+Test PostgreSQL module keys with:
+    zabbix_agentd -t <key>
+
+Print all key tests with:
+    zabbix_agentd -p | grep '^pg\.'
+
+All project sources are mapped in /vagrant
+
+Manage PostgreSQL server from:
+    http://localhost:8080/phpPgAdmin (postgres:postgres)
+
+MOTD
+
+echo -e "${BULLET} All done."
---
+++ vagrant/setup_ubuntu12.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+BULLET="==>"
+cd /vagrant
+
+# Install PostgreSQL package repo
+echo -e "${BULLET} Installing Zabbix, PostgreSQL and build tools..."
+if [[ ! -f /etc/apt/sources.list.d/pgdg.list ]]; then
+	echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+fi
+
+# Install Zabbix package repo
+[[ -f zabbix-release_2.2-1+precise_all.deb ]] || wget -nv http://repo.zabbix.com/zabbix/2.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_2.2-1+precise_all.deb
+dpkg -l zabbix-release > /dev/null || dpkg -i zabbix-release_2.2-1+precise_all.deb
+
+# Update package list and install
+apt-get -q update
+apt-get -y install zabbix-agent make gcc autoconf automake m4 libtool 	postgresql-9.4 postgresql-client-9.4 libpq-dev
+
+# Configure PostgreSQL
+echo -e "${BULLET} Configuring PostgreSQL server..."
+cat > /etc/postgresql/9.4/main/pg_hba.conf <<EOL
+local   all             all                                     trust
+host    all             all             127.0.0.1/32            trust
+host    all             all             ::1/128                 trust
+EOL
+/etc/init.d/postgresql reload
+
+# Build module
+echo -e "${BULLET} Building the libzbxpgsql agent module..."
+libtoolize >/dev/null
+aclocal >/dev/null
+autoheader >/dev/null
+automake --add-missing >/dev/null
+autoreconf >/dev/null
+./configure >/dev/null && make >/dev/null
+
+# Configure Zabbix Agent
+echo -e "${BULLET} Installing libzbxpgsql..."
+echo "LoadModule=libzbxpgsql.so" > /etc/zabbix/zabbix_agentd.d/libzbxpgsql.conf
+
+# Install add link to agent modules for built libzbxpgsql module
+[[ -d /usr/lib/modules ]] || mkdir /usr/lib/modules
+[[ -L /usr/lib/modules/libzbxpgsql.so ]] || ln -s /vagrant/src/.libs/libzbxpgsql.so /usr/lib/modules/libzbxpgsql.so
+
+# MOTD
+echo -e "${BULLET} Configuring message of the day..."
+cat > /etc/motd <<MOTD
+  _ _ _        _                               _ 
+ | (_) |__ ___| |____  ___ __   __ _ ___  __ _| |
+ | | | '_ \\_  / '_ \\ \\/ / '_ \\ / _\` / __|/ _\` | |
+ | | | |_) / /| |_) >  <| |_) | (_| \\__ \\ (_| | |
+ |_|_|_.__/___|_.__/_/\\_\\ .__/ \\__, |___/\\__, |_|
+                        |_|    |___/        |_|  
+
+            DΣVΣᄂӨPMΣПƬ VΛGЯΛПƬ BӨX
+
+Test PostgreSQL module keys with:
+    zabbix_agentd -t <key>
+
+Print all key tests with:
+    zabbix_agentd -p | grep '^pg\.'
+
+All project sources are mapped in /vagrant
+
+MOTD
+
+echo -e "${BULLET} All done."
