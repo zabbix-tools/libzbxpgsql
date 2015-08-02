fd86c58a8a0367d8a988477c802964f7c7019b4f
Ryan Armstrong <ryan@cavaliercoder.com>
2015-08-02 22:31:49 +0800

    Updated pg.db.discovery to use pg_get_discovery method

--- src/pg_database.c
+++ src/pg_database.c
@@ -57,69 +57,23 @@ ORDER BY 1;"
  * {
  *        "data":[
  *                {
- *                        "{#DATNAME}":"MyDatabase",
+ *                        "{#OID}":"1234",
+ *                        "{#DATABASE}":"MyDatabase",
+ *                        "{#ENCODING}":"UTF8",
  *                        "{#LC_COLLATE}":"en_US.UTF-8",
  *                        "{#LC_CTYPE}":"en_US.UTF-8",
  *                        "{#TEMPLATE}":"1|0",
- *                        "{#TABLESPACE":"pg_default"}]}
+ *                        "{#TABLESPACE":"pg_default",
+ *                        "{#DESCRIPTION}":"something or otheer"}]}
  */
 int    PG_DB_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
     int         ret = SYSINFO_RET_FAIL;                 // Request result code
     const char  *__function_name = "PG_DB_DISCOVERY";   // Function name for log file
-    struct      zbx_json j;                             // JSON response for discovery rule
-    
-    PGconn      *conn = NULL;
-    PGresult    *res = NULL;
-    
-    // Ignore tables that don't allow connections as we can't monitor them anyway!
-    char        *query = PGSQL_DISCOVER_DBS;
-    int         i = 0, count = 0;
     
     zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
-    
-    // Connect to PostreSQL
-    if(NULL == (conn = pg_connect(request)))
-        goto out;
-    
-    // Execute a query
-    res = pg_exec(conn, query);
-    if(PQresultStatus(res) != PGRES_TUPLES_OK) {
-        zabbix_log(LOG_LEVEL_ERR, "Failed to execute PostgreSQL query in %s() with: %s", __function_name, PQresultErrorMessage(res));
-        goto out;
-    }
-    
-    if(0 == (count = PQntuples(res))) {
-        zabbix_log(LOG_LEVEL_DEBUG, "No results returned for query \"%s\" in %s()", query, __function_name);
-        goto out;
-    }
-             
-    // Create JSON array of discovered objects
-    zbx_json_init(&j, ZBX_JSON_STAT_BUF_LEN);
-    zbx_json_addarray(&j, ZBX_PROTO_TAG_DATA);
-    
-    for(i = 0; i < count; i++) {
-        zbx_json_addobject(&j, NULL);
-        zbx_json_addstring(&j, "{#OID}", PQgetvalue(res, i, 0), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#DATABASE}", PQgetvalue(res, i, 1), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#ENCODING}", PQgetvalue(res, i, 2), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#LC_COLLATE}", PQgetvalue(res, i, 3), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#LC_CTYPE}", PQgetvalue(res, i, 4), ZBX_JSON_TYPE_STRING); 
-        zbx_json_addstring(&j, "{#OWNER}", PQgetvalue(res, i, 5), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#TABLESPACE}", PQgetvalue(res, i, 6), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#DESCRIPTION}", PQgetvalue(res, i, 7), ZBX_JSON_TYPE_STRING);
-        zbx_json_close(&j);         
-    }
-    
-    // Finalize JSON response
-    zbx_json_close(&j);
-    SET_STR_RESULT(result, strdup(j.buffer));
-    zbx_json_free(&j);
-    ret = SYSINFO_RET_OK;
-        
-out:
-    PQclear(res);
-    PQfinish(conn);
+ 
+    ret = pg_get_discovery(request, result, PGSQL_DISCOVER_DBS);
     
     zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __function_name);
     return ret;
