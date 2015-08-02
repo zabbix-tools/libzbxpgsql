eba82ea8eb6edaae777b32783521920db523c24c
Ryan Armstrong <ryan@cavaliercoder.com>
2015-08-02 22:44:35 +0800

    Updated pg.index.discovery to use pg_get_discovery method

--- src/pg_index.c
+++ src/pg_index.c
@@ -81,21 +81,17 @@ int    PG_INDEX_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
 {
     int         ret = SYSINFO_RET_FAIL;                 // Request result code
     const char  *__function_name = "PG_DB_DISCOVERY";   // Function name for log file
-    struct      zbx_json j;                             // JSON response for discovery rule
-    
-    PGconn      *conn = NULL;
-    PGresult    *res = NULL;
     
     char        query[MAX_STRING_LEN], buffer[MAX_STRING_LEN];
     char        *c = NULL;
-    int         i = 0, count = 0;
 
     char        *param_table = NULL, *param_schema = NULL;
     
     zabbix_log(LOG_LEVEL_DEBUG, "In %s()", __function_name);
 
     // build the query
-    c = strcat2(query, PGSQL_DISCOVER_INDEXES);
+    zbx_strlcpy(query, PGSQL_DISCOVER_INDEXES, sizeof(query));
+    c = query;
 
     // filter by schema name
     param_schema = get_rparam(request, PARAM_FIRST);
@@ -110,46 +106,10 @@ int    PG_INDEX_DISCOVERY(AGENT_REQUEST *request, AGENT_RESULT *result)
         zbx_snprintf(buffer, sizeof(buffer), " AND t.relname = '%s'", param_table);
         c = strcat2(c, buffer);
     }
-    
-    // Connect to PostgreSQL
-    if(NULL == (conn = pg_connect(request)))
-        goto out;
-    
-    // Execute a query
-    res = pg_exec(conn, query);
-    if(PQresultStatus(res) != PGRES_TUPLES_OK) {
-        zabbix_log(LOG_LEVEL_ERR, "Failed to execute PostgreSQL query in %s() with: %s", __function_name, PQresultErrorMessage(res));
-        goto out;
-    }
 
-    count = PQntuples(res);
+    // build results
+    ret = pg_get_discovery(request, result, query);
 
-    // Create JSON array of discovered objects
-    zbx_json_init(&j, ZBX_JSON_STAT_BUF_LEN);
-    zbx_json_addarray(&j, ZBX_PROTO_TAG_DATA);
-    
-    for(i = 0; i < count; i++) {
-        zbx_json_addobject(&j, NULL);        
-        zbx_json_addstring(&j, "{#OID}", PQgetvalue(res, i, 0), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#INDEX}", PQgetvalue(res, i, 1), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#DATABASE}", PQgetvalue(res, i, 2), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#SCHEMA}", PQgetvalue(res, i, 3), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#TABLE}", PQgetvalue(res, i, 4), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#OWNER}", PQgetvalue(res, i, 5), ZBX_JSON_TYPE_STRING);
-        zbx_json_addstring(&j, "{#ACCESS}", PQgetvalue(res, i, 6), ZBX_JSON_TYPE_STRING);
-        zbx_json_close(&j);         
-    }
-    
-    // Finalize JSON response
-    zbx_json_close(&j);
-    SET_STR_RESULT(result, strdup(j.buffer));
-    zbx_json_free(&j);
-    ret = SYSINFO_RET_OK;
-        
-out:
-    PQclear(res);
-    PQfinish(conn);
-    
     zabbix_log(LOG_LEVEL_DEBUG, "End of %s()", __function_name);
     return ret;
 }
