86323422faeff6067f03244150485ccc6842afbe
Ryan Armstrong <ryan@cavaliercoder.com>
2015-08-16 11:36:53 +0800

    Added limited monitoring role to all test scripts

--- docker/start_postgresql.sh
+++ docker/start_postgresql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PSQL_CREATE_ROLE="CREATE ROLE monitoring WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE;"
 
 # download and extract sample database
 [[ -f dellstore2-normal-1.0.tar.gz ]] || curl -LO http://pgfoundry.org/frs/download.php/543/dellstore2-normal-1.0.tar.gz
@@ -12,12 +13,23 @@ docker run -d -p 5492:5432 --name pg92 postgres:9.2
 docker run -d -p 5493:5432 --name pg93 postgres:9.3
 docker run -d -p 5494:5432 --name pg94 postgres:9.4
 
-sleep 3 # let the babies wake
+sleep 10 # let the babies wake
 
 # install sample databases
 psql -U postgres -h 192.168.59.103 -p 5484 < dellstore2-normal-1.0/dellstore2-normal-1.0.sql
+psql -U postgres -h 192.168.59.103 -p 5484 -c "${PSQL_CREATE_ROLE}"
+
 psql -U postgres -h 192.168.59.103 -p 5490 < dellstore2-normal-1.0/dellstore2-normal-1.0.sql
+psql -U postgres -h 192.168.59.103 -p 5490 -c "${PSQL_CREATE_ROLE}"
+
 psql -U postgres -h 192.168.59.103 -p 5491 < dellstore2-normal-1.0/dellstore2-normal-1.0.sql
+psql -U postgres -h 192.168.59.103 -p 5491 -c "${PSQL_CREATE_ROLE}"
+
 psql -U postgres -h 192.168.59.103 -p 5492 < dellstore2-normal-1.0/dellstore2-normal-1.0.sql
+psql -U postgres -h 192.168.59.103 -p 5492 -c "${PSQL_CREATE_ROLE}"
+
 psql -U postgres -h 192.168.59.103 -p 5493 < dellstore2-normal-1.0/dellstore2-normal-1.0.sql
+psql -U postgres -h 192.168.59.103 -p 5493 -c "${PSQL_CREATE_ROLE}"
+
 psql -U postgres -h 192.168.59.103 -p 5494 < dellstore2-normal-1.0/dellstore2-normal-1.0.sql
+psql -U postgres -h 192.168.59.103 -p 5494 -c "${PSQL_CREATE_ROLE}"
