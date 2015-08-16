d8bce0c622abe589f2cf5c986a7b9420bd76e48c
Ryan Armstrong <ryan@cavaliercoder.com>
2015-08-16 11:37:11 +0800

    Refactored tests script

--- docker/test.sh
+++ docker/test.sh
@@ -1,61 +1,43 @@
 #!/bin/bash
 THREADS=8
-
+ARGS="-threads ${THREADS} -iterations 1 -strict"
 export PGHOST=192.168.59.103
 export PGUSER="postgres"
 export PGDATABASE="postgres"
 
 # test v8.4
-export PGCONN="host=${PGHOST} user=${PGUSER} port=5484"
-zabbix_agent_bench \
-	-threads ${THREADS} \
-	-iterations 1 \
-	-strict \
+PGCONN="host=${PGHOST} user=${PGUSER} port=5484" \
+	zabbix_agent_bench ${ARGS} \
 	-keys ../fixtures/postgresql-8.4.keys \
 	|| exit 1
 
 # test v9.0
-export PGCONN="host=${PGHOST} user=${PGUSER} port=5490"
-zabbix_agent_bench \
-	-threads ${THREADS} \
-	-iterations 1 \
-	-strict \
+PGCONN="host=${PGHOST} user=${PGUSER} port=5490" \
+	zabbix_agent_bench ${ARGS} \
 	-keys ../fixtures/postgresql-8.4.keys \
 	|| exit 1
 
 # test v9.1
-export PGCONN="host=${PGHOST} user=${PGUSER} port=5491"
-zabbix_agent_bench \
-	-threads ${THREADS} \
-	-iterations 1 \
-	-strict \
+PGCONN="host=${PGHOST} user=${PGUSER} port=5491" \
+	zabbix_agent_bench ${ARGS} \
 	-keys ../fixtures/postgresql-9.1.keys \
 	|| exit 1
 
 # test v9.2
-export PGCONN="host=${PGHOST} user=${PGUSER} port=5492"
-zabbix_agent_bench \
-	-threads ${THREADS} \
-	-iterations 1 \
-	-strict \
+PGCONN="host=${PGHOST} user=${PGUSER} port=5492" \
+	zabbix_agent_bench ${ARGS} \
 	-keys ../fixtures/postgresql-9.2.keys \
 	|| exit 1
 
 # test v9.3
-export PGCONN="host=${PGHOST} user=${PGUSER} port=5493"
-zabbix_agent_bench \
-	-threads ${THREADS} \
-	-iterations 1 \
-	-strict \
+PGCONN="host=${PGHOST} user=${PGUSER} port=5493" \
+	zabbix_agent_bench ${ARGS} \
 	-keys ../fixtures/postgresql-9.2.keys \
 	|| exit 1
 
 # test v9.4
-export PGCONN="host=${PGHOST} user=${PGUSER} port=5494"
-zabbix_agent_bench \
-	-threads ${THREADS} \
-	-iterations 1 \
-	-strict \
+PGCONN="host=${PGHOST} user=${PGUSER} port=5494" \
+	zabbix_agent_bench ${ARGS} \
 	-keys ../fixtures/postgresql-9.2.keys \
 	|| exit 1
 
