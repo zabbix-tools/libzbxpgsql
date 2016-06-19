62dff85a5b4ef31fd1608fb73e1c25eee6cad839
Ryan Armstrong <ryan@cavaliercoder.com>
2016-06-19 15:18:52 +0800

    Moved packaging and testing scripts to libzbxpgsql-build project

--- docker/centos-7/zabbix-2.4/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rpm -ivh /root/libzbxpgsql/libzbxpgsql-${VERSION}-1.el7.x86_64.rpm || exit 1
-zabbix_agentd -t pg.modver | grep --color "libzbxpgsql ${VERSION}" || exit 1
--- docker/opensuse-13.2/zabbix-2.2/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-zypper \
-	--non-interactive \
-	--no-gpg-checks \
-	install \
-	/root/${PACKAGE}/${PACKAGE}-${VERSION}-1.sles13.$(uname -m).rpm \
-	|| exit 1
-
-zabbix_agentd -t pg.modver | grep --color "libzbxpgsql ${VERSION}" || exit 1
